ebc04554c2220c831cef42968263f7b34145cdb1
Eugene-Ba <eugenekotov.email@gmail.com>
2020-10-08 11:56:34 +0300

    Update db.php

--- php/db.php
+++ php/db.php
@@ -6,6 +6,6 @@ try {
 	$db = new PDO("mysql:host=localhost;dbname=news", $user, $password);
 	echo "<script>console.log('OK~')</script>";
 } catch (Exception $e) {
-	echo "<p> Извините, в данный момент сервер недоступен. </p>"+$e->getMessage();
+	echo $e;
 }
  ?>
